81203271369ecf0e8ff33d7fb12841c6383438c8
kwasi <kwasinti@google.com>
2020-01-01 22:46:43 -0800

    Export all compiled ml_metadata protos

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,3 @@
-export const FRONTEND: string;
+export * from './src/generated/src/apis/metadata/metadata_store_pb';
+export * from './src/generated/src/apis/metadata/metadata_store_service_grpc_web_pb';
+export * from './src/generated/src/apis/metadata/metadata_store_service_pb';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export const FRONTEND="frontend";
+export * from './generated/src/apis/metadata/metadata_store_pb';
+export * from './generated/src/apis/metadata/metadata_store_service_grpc_web_pb';
+export * from './generated/src/apis/metadata/metadata_store_service_pb';
